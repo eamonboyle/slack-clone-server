2dc987bb8c02d82015d84546f61cfa26b992d1ca
Eamon Boyle <eamon@watchtech.co.uk>
2019-01-07 12:01:48 +0000

    Updated to pull teams & username

--- resolvers/team.js
+++ resolvers/team.js
@@ -2,20 +2,27 @@ import formatErrors from '../formatErrors';
 import requiresAuth from '../permissions';
 
 export default {
+    Query: {
+        allTeams: requiresAuth.createResolver(async (parent, args, { models, user }) =>
+            models.Team.findAll({ owner: user.id }, { raw: true })),
+    },
     Mutation: {
         createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
             try {
-                await models.Team.create({ ...args, owner: user.id })
+                await models.Team.create({ ...args, owner: user.id });
                 return {
-                    ok: true
+                    ok: true,
                 };
             } catch (err) {
                 console.log(err);
                 return {
                     ok: false,
-                    errors: formatErrors(err)
+                    errors: formatErrors(err),
                 };
             }
-        })
-    }
+        }),
+    },
+    Team: {
+        channels: ({ id }, args, { models }) => models.Channel.findAll({ teamId: id }),
+    },
 };
